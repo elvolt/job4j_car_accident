4a9ea3062658c562d70521b06f12c253219fda74
elvolt <sidelevcs@gmail.com>
2021-09-14 15:54:15 +0300

    3. @ModelAttribute. Создание инцидента. [#261013 #202757]

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -6,20 +6,23 @@ import ru.job4j.accident.model.Accident;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 @Repository
 public class AccidentMem implements Store {
     private final Map<Integer, Accident> accidents = new HashMap<>();
-    private int size = 0;
+    private final AtomicInteger size = new AtomicInteger(0);
 
     public AccidentMem() {
-        IntStream.rangeClosed(0, 10)
-                .forEach(i -> {
-                    accidents.put(i,
-                            new Accident(size, "Name " + i, "Text " + i, "Address " + i));
-                    size++;
-                });
+        IntStream.rangeClosed(1, 10)
+                .forEach(i -> accidents.put(i, new Accident(
+                                size.incrementAndGet(),
+                                "Name " + i,
+                                "Text " + i,
+                                "Address " + i)
+                        )
+                );
     }
 
     @Override
@@ -30,7 +33,7 @@ public class AccidentMem implements Store {
     @Override
     public void saveAccident(Accident accident) {
         if (accident.getId() == 0) {
-            accident.setId(size);
+            accident.setId(size.incrementAndGet());
         }
         accidents.put(accident.getId(), accident);
     }
